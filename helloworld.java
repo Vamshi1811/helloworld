a117a3771928ead76025aaff3eb978d258383cfb
Vamshi1811 <56248860+Vamshi1811@users.noreply.github.com>
2019-12-22 11:58:37 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -4,14 +4,17 @@ class helloworld {
     public static void main(String[] args) {
     int x=5;
 String s = (String)"Hello!";
-try
+/*try
 {
         if(x==x)
         {
         }
         System.out.println("Hello, World!"); 
 int y=42/0;
-}
+}*/
+try { throw new java.io.CharConversionException();
+} catch (java.io.CharConversionException e) {
+} catch (java.io.IOException e) {}
 finally
 {
 }
