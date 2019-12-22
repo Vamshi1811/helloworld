d01fd381b71fa85b8005b42d853cb6cfcffe11ac
Vamshi1811 <56248860+Vamshi1811@users.noreply.github.com>
2019-12-22 11:46:05 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,11 +1,19 @@
+import java.io.*;
+import java.lang.String;
 class helloworld {
     public static void main(String[] args) {
-       int x=5;
-        int y=8;
+    int x=5;
+String s = (String)"Hello!";
+try
+{
         if(x==x)
         {
-            System.out.println(x);
         }
         System.out.println("Hello, World!"); 
+int y=42/0;
+}
+finally
+{
+}
     }
 }
