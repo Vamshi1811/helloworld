63db4562187a8852637ca451c94988c6fae0cf3b
Vamshi1811 <56248860+Vamshi1811@users.noreply.github.com>
2019-12-22 11:00:11 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,10 @@
 class helloworld {
     public static void main(String[] args) {
+        int x;
+        if(x=5)
+        {
+            
+        }
         System.out.println("Hello, World!"); 
     }
 }
